f215f7759751f8de8e204d957ac40be3db1bf952
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2025-07-20 23:01:12 -0500

    Slight update to error page, needs more styling.

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
 import Header from "./components/Header";
+import confusedPenguin from "./assets/confused-penguin.png";
+import "./styles/errorPage.css";
 
 const ErrorPage = () => {
   return (
     <div className="app-container-wrapper">
       <div className="app-container">
         <Header />
-        <h1>Oh no, this route doesn't exist!</h1>
+        <h1>Uh oh, this page doesn't exist!</h1>
         <Link to="/">
           You can go back to the home page by clicking here, though!
         </Link>
+        {/* <img
+          className="error-page-img"
+          src={confusedPenguin}
+          alt="confused-penguin"
+        /> */}
       </div>
     </div>
   );
