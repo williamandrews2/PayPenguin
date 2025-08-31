3ad0dc22ec65b8e9af2bf173edf7d19a70374ae7
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2025-08-30 23:40:11 -0500

    Removed unused useState and useEffect in BillChart.jsx.

--- src/components/BillChart.jsx
+++ src/components/BillChart.jsx
@@ -27,14 +27,6 @@ function BillChart() {
     "#DA70D6", // orchid
     "#00CED1", // dark turquoise
   ];
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 768);
-    window.addEventListener("resize", handleResize);
-    // remove the event listener on component unmount
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   return (
     <section className="monthly-expense-breakdown">
