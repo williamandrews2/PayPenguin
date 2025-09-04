123ab193c22bc939c296d56e2888fac038009e7a
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2025-09-03 22:12:26 -0500

    Updated hero image on home page.

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,7 +13,7 @@ export function HeroSection() {
           stress.
         </p>
 
-        <Link to="dashboard" className="get-started-button">
+        <Link to="dashboard" className="get-started-button" id="hero-image">
           Get Started
         </Link>
       </div>
